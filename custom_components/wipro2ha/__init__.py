b65411090d647675a27159dfef3960b9042ffb7d
hflocki <heiko.flocke@gmail.com>
2026-09-03 10:31:39 +0000

    Implement update listener for coordinator options
    Added an update listener to handle options changes for the coordinator.

--- custom_components/wipro2ha/__init__.py
+++ custom_components/wipro2ha/__init__.py
@@ -18,8 +18,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_start()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
+    # On options change, reload coordinator loop
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    coordinator: WiProDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    await coordinator.async_stop()
+    await coordinator.async_start()
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
